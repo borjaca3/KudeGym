f51a7f70f6cecec60592f266c20c28a81b1dc54d
pedrohdez <pedro16hermartina@gmail.com>
2024-04-15 17:17:23 +0200

    calendario y logo

---
+++ Api26/app/src/main/java/com/example/api26/Estadisticas.java
@@ -0,0 +1,47 @@
+package com.example.api26;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.app.DatePickerDialog;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
+
+public class Estadisticas extends AppCompatActivity {
+
+
+    @SuppressLint("MissingInflatedId")
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.estadisticas);
+
+    }
+    public void abrirCalendario(View view){
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog dpd = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+               String fecha = year + "-" + month + "-" + dayOfMonth;
+                //tv.setText(fecha);
+                // Show the selected date in a Toast
+                Context context = getApplicationContext();
+                CharSequence text = "Selected Date: " + fecha;
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        }, year, month, day);
+        dpd.show();
+
+
+    }
+}
